52cb0cd77ca518660918434574573d7d614cc784
farbs03 <cgeraldfarber@icloud.com>
2021-03-05 04:43:25 -0500

    Big boy changes inbound

--- app.py
+++ app.py
@@ -1,35 +1,15 @@
+from werkzeug.utils import cached_property
 from flask import Flask, request, jsonify, make_response
-from flask_restplus import Api, Resource, fields
-from sklearn.externals import joblib
+from flask_restful import Api, Resource
 import numpy as np
+import pandas as pd
+import model
 import sys
 
-flask_app = Flask(__name__)
-app = Api(app = flask_app, 
-		  version = "1.0", 
-		  title = "Iris Plant identifier", 
-		  description = "Predict the type of iris plant")
+app = Flask(__name__)
+api = Api(app)
 
-name_space = app.namespace('prediction', description='Prediction APIs')
-
-model = app.model('Prediction params', 
-				  {'sepalLength': fields.Float(required = True, 
-				  							   description="Sepal Length", 
-    					  				 	   help="Sepal Length cannot be blank"),
-				  'sepalWidth': fields.Float(required = True, 
-				  							   description="Sepal Width", 
-    					  				 	   help="Sepal Width cannot be blank"),
-				  'petalLength': fields.Float(required = True, 
-				  							description="Petal Length", 
-    					  				 	help="Petal Length cannot be blank"),
-				  'petalWidth': fields.Float(required = True, 
-				  							description="Petal Width", 
-    					  				 	help="Petal Width cannot be blank")})
-
-classifier = joblib.load('classifier.joblib')
-
-@name_space.route("/")
-class MainClass(Resource):
+class GenerateInsights(Resource):
 
 	def options(self):
 		response = make_response()
@@ -37,19 +17,17 @@ class MainClass(Resource):
 		response.headers.add('Access-Control-Allow-Headers', "*")
 		response.headers.add('Access-Control-Allow-Methods', "*")
 		return response
-
-	@app.expect(model)		
+	
 	def post(self):
 		try: 
 			formData = request.json
-			data = [val for val in formData.values()]
-			prediction = classifier.predict(np.array(data).reshape(1, -1))
-			types = { 0: "Iris Setosa", 1: "Iris Versicolour ", 2: "Iris Virginica"}
+			formData = pd.json_normalize(formData)
+			model.addData(formData)
 			response = jsonify({
 				"statusCode": 200,
 				"status": "Prediction made",
-				"result": "The type of iris plant is: " + types[prediction[0]]
-				})
+				"result": model.run()
+			})
 			response.headers.add('Access-Control-Allow-Origin', '*')
 			return response
 		except Exception as error:
@@ -58,3 +36,8 @@ class MainClass(Resource):
 				"status": "Could not make prediction",
 				"error": str(error)
 			})
+
+api.add_resource(GenerateInsights, '/')
+
+if __name__ == "__main__":
+	app.run(debug=True)
