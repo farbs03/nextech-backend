5dcc904ae55bdb87695e75801cdfbc52c7223f7a
farbs03 <cgeraldfarber@icloud.com>
2021-03-17 15:50:12 -0400

    improved the model

--- model.py
+++ model.py
@@ -1,24 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_regression
+from sklearn.linear_model import LinearRegression
 import math
 
-# feature selection
-def select_features(X_train, y_train, X_test):
-    # configure to select all features
-    fs = SelectKBest(score_func=f_regression, k='all')
-    # learn relationship from training data
-    fs.fit(X_train, y_train)
-    # transform train input data
-    X_train_fs = fs.transform(X_train)
-    # transform test input data
-    X_test_fs = fs.transform(X_test)
-    return X_train_fs, X_test_fs, fs
+data = pd.read_csv("studentdata.csv", delimiter=",")
 
 def addData(request):
-    data = pd.read_csv("studentdata.csv", delimiter=",")
+    global data
     formData = pd.json_normalize(request)
     idx = 0
     for entry in formData.values:
@@ -31,26 +19,29 @@ def addData(request):
     
 def run(request):
     addData(request)
-    data = pd.read_csv("studentdata.csv", delimiter=",")
-
-    X = data[["Work", "School", "Life", "Exercise"]].values
+    global data
+    features = ["Work", "School", "Life", "Exercise"]
+    X = data[features].values
     y = data["Happiness"].values
+    model = LinearRegression()
+    model.fit(X, y)
+    importance = model.coef_
 
-    # split into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    # feature selection
-    X_train_fs, X_test_fs, fs = select_features(X_train, y_train, X_test)
-    # what are scores for the features
-    idx = 0
-    total = 0
-    most_important = max(fs.scores_)
-    for i in range(len(fs.scores_)):
-        if(fs.scores_[i] == most_important):
-            idx = i
-        total += fs.scores_[i]
-    #Show the scores
-    most_important = round(most_important, 2)
-    return(f"{data.columns[idx + 1]} tasks contributed the most to your happiness!")
+    most_important = ("", 0)
+    most_harmful = ("", 10000)
+    
+    for i, j in enumerate(importance):
+        if j > most_important[1]:
+            most_important = (features[i], j)
+        if j < most_harmful[1]:
+            most_harmful = (features[i], j)
+
+    return(
+        f"""
+        {most_important[0]} tasks contributed the most to your happiness
+        {most_harmful[0]} tasks took the largest toll on your happiness
+        """
+        )
 
 
 
