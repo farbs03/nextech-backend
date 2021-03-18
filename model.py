b50106922b0d24c5c8e615753207bf39507fad90
farbs03 <cgeraldfarber@icloud.com>
2021-03-17 21:27:48 -0400

    improved the model

--- model.py
+++ model.py
@@ -37,7 +37,7 @@ def run(request):
             most_harmful = (features[i], j)
 
     return(
-        f"{most_important[0]} tasks contributed the most to your happiness ({round(most_important[1], 2)}) and {most_harmful[0].lower()} tasks took the largest toll on it ({round(most_harmful[1], 2)})"
+        f"{most_important[0]} tasks contributed the most to your happiness and {most_harmful[0].lower()} tasks took the largest toll on it"
     )
 
 
